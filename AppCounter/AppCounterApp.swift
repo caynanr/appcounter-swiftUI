9645897c3190c82da701de4fe960a67c92a9dd56
Caynan <caynanramos@gmail.com>
2021-12-18 14:46:35 -0300

    Initial Commit

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by Caynan Alves Ramos on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
